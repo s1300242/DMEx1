8875d22d5be0213695d26813f3699d829cc9a6d7
s1300242 <136494863+s1300242@users.noreply.github.com>
2023-06-14 03:24:00 +0900

    Won/lost message added

--- ex1.c
+++ ex1.c
@@ -17,5 +17,9 @@ char name[20];
     printf("Die 1: %d\n",d1);
     printf("Die 2: %d\n",d2);
     printf("Total value: %d\n",d1+d2);
+  if(d1+d2>=7){
+    printf("%s won!\n",name);
+  }
+  else printf("%s lost!\n",name);
     return 0;
 }

c7633762a76861caf55fab529a3ad7ef00be332d
s1300242 <136494863+s1300242@users.noreply.github.com>
2023-06-14 03:06:21 +0900

    Create ex1.c

---
+++ ex1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+  printf("Rolling dice...\n");
+  srand((unsigned int) time(NULL));
+    int x=rand();
+    int y=rand();
+    int d1=x%6+1;
+    int d2=y%6+1;
+    printf("Die 1: %d\n",d1);
+    printf("Die 2: %d\n",d2);
+    printf("Total value: %d\n",d1+d2);
+    return 0;
+}
